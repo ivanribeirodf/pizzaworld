d2c1d9a358751c5e74ac2d6ccb748781664b7f08
IvanPix <ivan@pix.com.br>
2023-12-29 17:13:10 -0300

    Cadastro de produtos e categorias com foto no backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,34 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController} from './controllers/user/DetailUserController'
+
+import { CreateCategoryController} from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { CreateProductController } from './controllers/produtc/CreateProductController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
-const router = Router();
+import uploadConfig from './config/multer'
 
+const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"))
+
+//-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthUserController().handle)
-router.get('/listUser', isAuthenticated, new DetailUserController().handle)
+router.get('/inforUser', isAuthenticated, new DetailUserController().handle)
+
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+
 
 export { router }
